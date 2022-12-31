3a40fafda167246136104b834799a73350f5cf91
hamza <hamzah@paritie.com>
2022-12-31 10:20:45 +0100

    worked on wishlist on the user dashboard

--- src/pages/Dashboard/WishList/WishList.jsx
+++ src/pages/Dashboard/WishList/WishList.jsx
@@ -9,6 +9,7 @@ import {
   ClickableToast,
   toastOptions
 } from "../../../components/Featured/ProductItem";
+import empty from "../../../../public/images/empty.png";
 
 const WishList = () => {
   const { user, setUser } = useUser();
@@ -48,61 +49,79 @@ const WishList = () => {
         pageTitle={`Wish Lists (${whishLists?.length || 0})`}
         // setHandleNotData={setHandleNotData}
       />
-      <div
-        className="wishlist"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 20,
-          marginTop: 20
-        }}
-      >
-        {whishLists.map((item, idx) => (
-          <div
-            key={idx}
-            className="item"
-            style={{
-              display: "flex",
-              gap: 20,
-              justifyContent: "space-between",
-              background: "white",
-              padding: "30px 20px"
-            }}
-          >
-            <div style={{ display: "flex", gap: 10 }}>
-              <img src={item.images[0]} style={{ width: 100, height: 100 }} />
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <p>
-                  <span style={{ fontWeight: 600 }}>{item.product_name}</span>
-                  {" - "}
-                  <span>{item.description}</span>
-                </p>
-                <div style={{ marginTop: "auto" }}>
-                  <p style={{ fontWeight: 600 }}>
-                    {currencyFormater(item.price)}
+      {whishLists.length ? (
+        <div
+          className="wishlist"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 20,
+            marginTop: 20
+          }}
+        >
+          {whishLists.map((item, idx) => (
+            <div
+              key={idx}
+              className="item"
+              style={{
+                display: "flex",
+                gap: 20,
+                justifyContent: "space-between",
+                background: "white",
+                padding: "30px 20px"
+              }}
+            >
+              <div style={{ display: "flex", gap: 10 }}>
+                <img src={item.images[0]} style={{ width: 100, height: 100 }} />
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <p>
+                    <span style={{ fontWeight: 600 }}>{item.product_name}</span>
+                    {" - "}
+                    <span>{item.description}</span>
                   </p>
-                  <p style={{ textDecoration: "line-through" }}>2,000</p>
+                  <div style={{ marginTop: "auto" }}>
+                    <p style={{ fontWeight: 600 }}>
+                      {currencyFormater(item.price)}
+                    </p>
+                    <p style={{ textDecoration: "line-through" }}>2,000</p>
+                  </div>
                 </div>
               </div>
+              <div className="grid">
+                <button
+                  onClick={() => handleAddToCart(item)}
+                  className="whishlist-buy-btn"
+                >
+                  BUY NOW
+                </button>
+                <button
+                  onClick={() => removeWhishList(item._id)}
+                  className="wishlist-remove-btn"
+                >
+                  <RiDeleteBin4Fill size={20} />
+                  <span>REMOVE</span>
+                </button>
+              </div>
             </div>
-            <div className="grid">
-              <button
-                onClick={() => handleAddToCart(item)}
-                className="whishlist-buy-btn"
-              >
-                BUY NOW
-              </button>
-              <button
-                onClick={() => removeWhishList(item._id)}
-                className="wishlist-remove-btn"
-              >
-                <RiDeleteBin4Fill size={20} />
-                <span>REMOVE</span>
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <div
+          style={{
+            background: "white",
+            display: "grid",
+            placeContent: "center",
+            paddingBottom: 30,
+            textAlign: "center",
+            marginTop: 10
+          }}
+        >
+          <img src={empty} style={{ width: 200, marginTop:20 }} />
+          <p style={{ fontSize: 12, marginTop: -5, marginBottom: 15 }}>
+            Items added to wishlists are <br /> displayed here.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
